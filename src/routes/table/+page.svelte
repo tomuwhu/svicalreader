2714f97df036dafa443a461a4ce178cf6dc5650d
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-02-14 16:03:18 +0100

    .

--- src/routes/table/+page.svelte
+++ src/routes/table/+page.svelte
@@ -5,7 +5,7 @@
             , firstWeekday = new Date(year, month, 1).getDay()
             , lastDateOfMonth = new Date(year, month + 1, 0).getDate()
             , offsetDate = this.getDate() + firstWeekday - 1
-            , index = 1 // start index at 0 or 1, your choice
+            , index = 0
             , weeksInMonth = index + Math.ceil((lastDateOfMonth + firstWeekday - 7) / 7)
             , week = index + Math.floor(offsetDate / 7)
         ;
@@ -22,7 +22,7 @@
         month: date.getMonth() + 1,
         day: date.getDate(),
         weekday: (date.getDay() + 6) % 7,
-        week: date.getWeekOfMonth() - 1
+        week: date.getWeekOfMonth()
     })
     const gentimeobj = (date) => ({
         hour: date.getHours(),
